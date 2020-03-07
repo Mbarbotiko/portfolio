d83471d747b2cdd6b713071e19c16f3ca30ea6ea
Margarita Barbotiko <mbarbotiko@gmail.com>
2020-03-07 10:35:11 -0600

    adding first component header

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './header.scss';
+
+function Header() {
+  return (
+      <header className="App-header">
+        <h1>M | B</h1>
+        <p>Let's build something great together.</p>
+      </header>
+  );
+}
+
+export default Header;

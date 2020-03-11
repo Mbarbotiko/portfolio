2143a48bb486342b2f03ad9c224aed343b877019
Margarita Barbotiko <mbarbotiko@gmail.com>
2020-03-10 19:45:51 -0500

    removing global variable and moving it into state index, adding additional button with function to go backwards

--- src/App.js
+++ src/App.js
@@ -8,61 +8,85 @@ import NavButton from './components/nav-button/nav-button';
 import projects from './data/projects';
 import './components/cc-card/cc-card.scss';
 
-let position = 0;
+//let position = 0;
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    
     this.state = {
-      bgColor: projects[position].bgColor,
-      bgOverlay: projects[position].bgOverlay,
-      borderColor: projects[position].borderColor,
-      image: projects[position].image,
-      header: projects[position].title,
-      description: projects[position].description,
-     
+      index: 0,
+      title: projects[0].title,
+      description: projects[0].description,
+      image: projects[0].image,
+      bgColor: projects[0].bgColor,
+      borderColor: projects[0].borderColor
+
     };
   }
 
-  nextProject() {
-    position++
-    if (position === projects.length) {
-      position = 0;
+  nextProject = () => {
+    let newIndex = this.state.index
+    newIndex++
+    if (newIndex === projects.length) {
+      newIndex = 0;
     }
-    this.setState({
-      bgColor: projects[position].bgColor,
-      bgOverlay: projects[position].bgOverlay,
-      borderColor: projects[position].borderColor,
-      image: projects[position].image,
-      header: projects[position].title,
-      description: projects[position].description
-    });
-    
+
+    this.setState(
+      {
+        index: newIndex,
+        title: projects[newIndex].title,
+        description: projects[newIndex].description,
+        image: projects[newIndex].image,
+        bgColor: projects[newIndex].bgColor,
+        borderColor: projects[newIndex].borderColor
+      }
+    )
+
+  }
+
+  previousProject = () => {
+    let newIndex = this.state.index
+    newIndex--
+    if (newIndex ===-1) {
+      newIndex = projects.length-1;
+    } 
+    this.setState(
+      {
+        index: newIndex,
+        title: projects[newIndex].title,
+        description: projects[newIndex].description,
+        image: projects[newIndex].image,
+        bgColor: projects[newIndex].bgColor,
+        borderColor: projects[newIndex].borderColor
+      }
+    )
 
   }
 
   render() {
-    
+
     return (
-      <div className={`App ${this.state.bgColor}
-       ${this.state.borderColor} ${this.state.bgOverlay}
+      <div className={`App ${this.state.bgColor} ${this.state.borderColor}
        `}>
+
         <Header />
-        <img className ='project-image' src={this.state.image}/>
-          <div className='card' >
-            <h2>{this.state.header}</h2>
-            <p>{this.state.description}</p>
-            <NavButton onClick={() => this.nextProject()
-        } />
-          </div>
-      
+        <img className='project-image' src={this.state.image} />
+        <div className='card' >
+        <NavButton onClick={this.previousProject} />
+          <h2>
+            {this.state.title}
+          </h2>
+          <p>{this.state.description}</p>
+
+          <NavButton onClick={this.nextProject} />
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
-  
+
 }
 
 
